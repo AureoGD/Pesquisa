74a0d6ce0a378ebcf795d681e64191858a665563
Unknown <aureogd@gmail.com>
2018-07-31 15:46:28 -0300

    Modificação 4
    Modificação na restrição de saída, no plot do controlador

--- algoritmo_controlador.m
+++ algoritmo_controlador.m
@@ -1,5 +1,5 @@
 clc
-clearvars x y x_e i ref t u e U du e e_rms J id dx
+clearvars x y x_e i ref t u e U du e_rms J id dx
 close all
 Tsimu = 5;
 Nint = Tsimu/Ts;
@@ -16,19 +16,19 @@ id(1:Nint)= 0;
 J(1:Nint)= 0;
 
 
-for i = 1:150
-    ref(i)= 350*i/150;
-    %ref(i) = 100;
-end
-
-for i = 1:200
-   ref(i+150) = 350;
-end
-    
-for i = 1:150
-    ref(i+350)= -350*i/150 +350;
-   %ref(i+350)= -120;
-end
+% for i = 1:150
+%     ref(i)= 350*i/150;
+%     %ref(i) = 100;
+% end
+% 
+% for i = 1:200
+%    ref(i+150) = 350;
+% end
+%     
+% for i = 1:150
+%     ref(i+350)= -350*i/150 +350;
+%    %ref(i+350)= -120;
+% end
 i = 0;
 
 for k = 2:Nint
@@ -38,6 +38,7 @@ for k = 2:Nint
  
     dx(k) = x(k) - x(k-1);
     e(k)  = y(k) - ref(k);
+    
     %obtenção da derivada do estado, do erro, e montagem do vetor x aumentado
     x_a    = [dx(k); -e(k)];% este sinal deveria ser positivo
     
@@ -64,29 +65,46 @@ end
 
 t = 0:(Nint-1);
 t = t*Ts;
+
+subplot(2,2,1)
 plot(t,y(1,:))
 hold on
 plot(t,ref(1,:))
+legend
+leg1 = legend('reference','out');
+set(leg1,'Interpreter','latex');
+set(leg1,'FontSize',10);
+set(leg1,'Location','southeast');
 title('Output')
 
-figure()
+
+subplot(2,2,2)
+plot(t,U(1,:))
+title('Control signal u')
+
+subplot(2,2,3)
 plot(t,dx(1,:))
-title('Delta x')
+title('\Delta x')
 
-figure()
+subplot(2,2,4)
 plot(t,du(1,:))
-title('Delta U')
+title('\Delta u')
 
-figure()
-e_rms(1:Nint)= 0;
-for i=1:Nint
-    e_rms(i) = sqrt(e(i)^2); 
-end
-plot(t,e_rms(1,:))
-title('erro rms')
 
-figure()
-plot(t,id(1,:))
 
-figure()
-plot(t,J(1,:))
+
+
+% 
+% figure()
+% e_rms(1:Nint)= 0;
+% for i=1:Nint
+%     e_rms(i) = sqrt(e(i)^2); 
+% end
+% plot(t,e_rms(1,:))
+% title('erro rms')
+% 
+% figure()
+% plot(t,id(1,:))
+% 
+% % figure()
+% plot(t,J(1,:))
